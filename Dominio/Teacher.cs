db5ed45d7e55ba7b06d1e5d22f9c5d6acedd932d
nicorodriguez66 <nicorodriguez66@gmail.com>
2017-10-15 12:17:27 -0300

    Teacher
    Se crea la clase teacher

---
+++ Dominio/Teacher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Teacher : person
+    {
+        public Teacher()
+        {
+            name = "";
+            surname = "";
+            //subjects = null;
+        }
+        public void EditTeacher()
+        {
+
+        }
+        public void DeleteTeacher()
+        {
+            //subjects.Clear();
+        }
+    }
+}
