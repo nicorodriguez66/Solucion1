657ff3d5278c02cc65bc234b52ad2be8632e0392
nicorodriguez66 <nicorodriguez66@gmail.com>
2017-10-15 13:48:01 -0300

    Subject

---
+++ Dominio/Subject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class subject
+    {
+        private string name { get; set; }
+        private string code { get; set; }
+        private List<Student> students;
+        private List<Teacher> teachers;
+        public subject()
+        {
+            students = null;
+            teachers = null;
+            name = "empty";
+            code = "-1";
+        }
+        public void EditSubject()
+        {
+
+        }
+        public void DeleteSubject()
+        {
+            students.Clear();
+            teachers.Clear();
+        }
+
+    }
+}
