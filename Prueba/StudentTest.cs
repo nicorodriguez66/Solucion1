d2fb5d083d75688f879120abfc9684fd60ed3634
nicorodriguez66 <nicorodriguez66@gmail.com>
2017-10-15 10:59:39 -0300

    Test & Dominio
    Se agrega el proyecto de prueba con tests unitarios basicos sobre clases del dominio aun no creadas para usar el metodo TDD.
    Ademas se agrega el proyecto Dominio que contendrá las clases de core con la logica del problema

---
+++ Prueba/StudentTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Prueba
+{
+    [TestClass]
+    public class StudentTest
+    {
+        [TestMethod]
+        public void StudentBuilderTest()
+        {
+            Student student = new Student();
+        }
+    }
+}
