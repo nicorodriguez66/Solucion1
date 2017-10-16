0a138891c4c14b1f6a40612c449e90395cab0ae3
nicorodriguez66 <nicorodriguez66@gmail.com>
2017-10-15 21:02:35 -0300

    Alumno ABM - Final

---
+++ Dominio/Subject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Dominio
+{
+    public class Subject
+    {
+        private string name { get; set; }
+        private string code { get; set; }
+        private List<Student> students;
+        private List<Teacher> teachers;
+        public Subject()
+        {
+            students = null;
+            teachers = null;
+            name = "empty";
+            code = "-1";
+        }
+        public void EditSubject()
+        {
+
+        }
+        public void DeleteSubject()
+        {
+            students.Clear();
+            teachers.Clear();
+        }
+
+    }
+}
