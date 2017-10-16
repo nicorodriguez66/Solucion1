124591cbf7797009e5ab199c1e1ba3d99eb63a2c
nicorodriguez66 <nicorodriguez66@gmail.com>
2017-10-16 01:49:45 -0300

    Revert "Subject Module - Create"
    This reverts commit ba53e3e7eee64b6b893e6efdd699e2f573eb6c49.

---
+++ Dominio/ERPSystem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class ERPsystem
+    {
+        private List<Teacher> allteachers;
+        private List<Student> allstudents;
+        private List<Subject> allsubjects;
+        private List<Van> allvans;
+
+        public ERPsystem()
+        {
+            allteachers = new List<Teacher>();
+            allsubjects = new List<Subject>();
+            allstudents = new List<Student>();
+            allvans = new List<Van>();
+        }
+        public List<Teacher> showallteachers()
+        {
+            return allteachers;
+        }
+        public List<Student> showallstudents()
+        {
+            return allstudents;
+        }
+        public List<Subject> showallsubjects()
+        {
+            return allsubjects;
+        }
+        public List<Van> showallvans()
+        {
+            return allvans;
+        }
+        public Student searchStudent(int Number)
+        {
+            Student found = null;
+            foreach (Student element in allstudents)
+            {
+                if (element.GetNumber().Equals(Number))
+                    found = element;
+            }
+            return found;
+        }
+    }
+}
---
+++ Dominio/Van.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Van
+    {
+        private int capacity;
+        public Van()
+        {
+            capacity = 0;
+        }
+        public void EditVan()
+        {
+
+        }
+        public void DeleteVan()
+        {
+
+        }
+        public void CalcularRuta()
+        {
+
+        }
+    }
+}
